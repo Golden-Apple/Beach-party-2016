bea929989c11fe7cdd38b75a98cf31944ade5635
M-065 <kshimabukuro@M-065.local>
2016-06-09 10:22:02 +0900

    Update info
    項目のレイアウトをスマホ対応にした

--- resources/views/layouts/info.blade.php
+++ resources/views/layouts/info.blade.php
@@ -7,17 +7,23 @@
       <h2>イベント詳細∧( 'Θ' )∧</h2>
     </div>
 
-    <div class="info-contents">
-      <ul class="list-group" >
-        <li class="list-group-item">日時: 7月9日(土) 15:00 ~ 19:00</li>
-        <li class="list-group-item">場所: うみそら公園(仮)</li>
-      </ul>
+    <div class="container info-contents">
+      <div class="row">
+        <ul class="col-md-8 list-group" >
+          <li class="list-group-item">日時: 7月9日(土) 15:00 ~ 19:00</li>
+          <li class="list-group-item">場所: うみそら公園(仮)</li>
+          <li class="list-group-item">参加費: きっと無料!</li>
+          <li class="list-group-item">持ち物: 特に無いけど、「これが飲みたい」ってのがあったら自由に持ってきてもいいのよ。瓶はNG</li>
+        </ul>
+      </div>
 
-      <div class="accessmap">
+      <div class="row accessmap">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1714.4213056853953!2d127.66521147950057!3d26.221288544008388!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e569922f08e25b%3A0x27441ac8cedca253!2z44CSOTAwLTAwMzcg5rKW57iE55yM6YKj6KaH5biC6L6777yT5LiB55uu77yT4oiS77yRIOazouOBruS4iuOBhuOBv-OBneOCieWFrOWckg!5e0!3m2!1sja!2sjp!4v1465373789056" width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
 
-      <button type="button" name="button" class="btn btn-primary btn-lg">お問い合わせ</button>
+      <div class="row">
+        <button type="button" name="button" class="btn btn-primary btn-lg">お問い合わせ</button>
+      </div>
     </div>
   </div>
 
