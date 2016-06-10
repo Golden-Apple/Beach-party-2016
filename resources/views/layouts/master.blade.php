741619a32c613fdfb86c27bc8fc73d88de5ec4a4
M-066 <skakazu@M-066.local>
2016-06-10 11:55:37 +0900

    1P風になったけど近い

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -45,7 +45,7 @@
 						<ul class="nav navbar-nav">
 							<li><a href="/">TOP</a></li>
 							<li><a href="/info">詳細</a></li>
-							<li><a href="/entry">申し込みフォーム</a></li>
+							<li><a href="#entry">申し込みフォーム</a></li>
 							<li><a href="/contact">問い合わせ</a></li>
 						</ul>						
 					</div>
@@ -54,6 +54,7 @@
 		</header>
 		
 	        @yield('content')
-
+	        @yield('entry')
+			
 	</body>
 </html>
