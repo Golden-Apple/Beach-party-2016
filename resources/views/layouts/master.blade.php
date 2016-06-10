34227c81f108296753a69cfe13184563a4840700
M-065 <kshimabukuro@M-065.local>
2016-06-10 14:30:38 +0900

    fix
    navがアホみたいなことになってた

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,14 +47,6 @@
 							</ul>
 						</div>
 					</div>
-					<div class="collapse navbar-collapse" id="menu">
-						<ul class="nav navbar-nav">
-							<li><a href="/">TOP</a></li>
-							<li><a href="/info">詳細</a></li>
-							<li><a href="#entry">申し込みフォーム</a></li>
-							<li><a href="/contact">問い合わせ</a></li>
-						</ul>
-					</div>
 				</div>
 			</nav>
 		</header>
