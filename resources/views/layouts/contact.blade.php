16a65b590fea84fe6670fab73fcb683245f7e045
M-065 <kshimabukuro@M-065.local>
2016-06-13 10:13:06 +0900

    Update MapStyle
    マップのデザインを変えました。

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -1,14 +1,12 @@
-@extends('layouts.master')
+@section('contact')
 
-@section('content')
-
-  <div class="container contact">
-    <div class="contact-header">
-      <h2>お問い合わせふぉーむ。</h2>
-    </div>
-    <div class="contact-content">
-      ふぉーむだお
-    </div>
-  </div>
+	<div class="container contact">
+		<div class="contact-header">
+			<h2>お問い合わせふぉーむ。</h2>
+		</div>
+		<div class="contact-content">
+			ふぉーむだお
+		</div>
+	</div>
 
 @endsection
