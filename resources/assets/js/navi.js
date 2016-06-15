25b3b969bab07156d673fd3dda681e3bf2b0d0a4
kabotya <kiyono.shima@gmail.com>
2016-06-15 18:25:35 +0900

    Update menu
    toggleメニューを追加しました

--- resources/assets/js/navi.js
+++ resources/assets/js/navi.js
@@ -9,7 +9,8 @@ class Nav extends React.Component {
 	render() {
 		return(
 			<div className="nav-field">
-				<ul className="nav-list">
+				<div id="toggle"><a href="#">menu</a></div>
+				<ul id="nav-list">
 					<li className="nav-title">びーちぱーりぃ</li>
 					<li><a href="/">top</a></li>
 					<li><a href="#info">info</a></li>
