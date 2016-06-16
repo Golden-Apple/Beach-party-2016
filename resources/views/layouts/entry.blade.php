1dbcbbc4be9a71eeb506c3d67956597f48dce82c
kabotya <kiyono.shima@gmail.com>
2016-06-16 11:42:03 +0900

    Change contents padding
    ちょっと気持ち悪くなるのでpaddingを修正しました

--- resources/views/layouts/entry.blade.php
+++ resources/views/layouts/entry.blade.php
@@ -4,6 +4,4 @@
 	<iframe src="https://docs.google.com/a/veincarry.jp/forms/d/1vZQpPNhcpJKXWDx3wBMgnAX3ZU1SfknLNb1bFczMH-s/viewform?embedded=true" width="100%" height="1100" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます...</iframe>
 </div>
 
-<hr>
-
 @endsection
