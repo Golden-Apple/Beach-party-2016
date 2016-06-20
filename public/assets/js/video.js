7fbdec22a69b808db20f95add23cbef29fad867c
シオン <skakazu@M-066.local>
2016-06-20 14:56:01 +0900

    topの画像、他css

--- public/assets/js/video.js
+++ public/assets/js/video.js
@@ -1,8 +1,11 @@
 $(function(){
 	$('.top-image').vegas({
 		slides: [
-	    	{ src: '/assets/vegas/beach.jpg' },
-	   		{ src:  '/assets/vegas/beach2.jpg'}
+	    	// { src: '/assets/vegas/beach.jpg' },
+	   		// { src:  '/assets/vegas/beach2.jpg'},]
+	    	{ src: '/assets/image/beach.jpg' },
+	   		{ src:  '/assets/image/beach2.jpg'},
+	   		{ src:  '/assets/image/beach3.jpg'}
 		],
 		overlay: '/assets/vegas/overlays/01.png'
 	});
