b11fdcaf347bfe0ab4748f792b7c56aeb254f5d6
シオン <skakazu@M-066.local>
2016-06-20 16:18:53 +0900

    topのcss

--- resources/views/layouts/top.blade.php
+++ resources/views/layouts/top.blade.php
@@ -14,7 +14,8 @@
 	</div>
 	<div class="container visible-xs top-xs-image col-xs-12">
 		<div class="top-xs-contents">
-			<p>CRUD Lab. BEACH PARTY 2016!!!</p>
+			<p>CRUD Lab.</p>
+			<p>BEACH PARTY 2016!!!</p>
 			<p>2016/7/9(sat)</p>
 			<p>波の上うみそら公園<span class="icon-158"></span>
 			</p>
